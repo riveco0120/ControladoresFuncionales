57b3432ba841fc3804a7042cb393afde65eb6053
riveco0120 <richardsk.vellojin@gmail.com>
2022-06-08 08:09:03 -0500

    Repositorio inicial

---
+++ src/main/java/com/sofka/demoControladoressFuncionales/DemoControladoressFuncionalesApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.demoControladoressFuncionales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoControladoressFuncionalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoControladoressFuncionalesApplication.class, args);
+	}
+
+}
